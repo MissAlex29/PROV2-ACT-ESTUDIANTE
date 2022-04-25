845427772c622e21eecd08581e915c522c1d07df
MissAlex29 <92283607+MissAlex29@users.noreply.github.com>
2022-04-25 16:30:19 -0500

    Update Cannon.js

--- Cannon.js
+++ Cannon.js
@@ -1,9 +1,3 @@
 class Cannon {
-    constructor(x,y,width,height,angle){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.angle = angle;
-    }
+    
 }
